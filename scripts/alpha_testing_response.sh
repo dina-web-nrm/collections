163ac0b437f2b57cee80be763637d3349b4caaa0
Ingimar Erlingsson <inkimar@gmail.com>
2017-11-27 15:54:50 +0100

    Modify, script 'alpha_testing_response.sh'. Using EXPECTED_RESULT

--- scripts/alpha_testing_response.sh
+++ scripts/alpha_testing_response.sh
@@ -8,9 +8,11 @@ RESULT=`curl --data "grant_type=password&client_id=${client_id}&username=${usern
 TOKEN=`echo $RESULT | sed 's/.*access_token":"//g' | sed 's/".*//g'`
 
 RESPONSE=`curl -H "Authorization: bearer $TOKEN" ${ALPHA_SERVER}/collections/api/v01`
+EXPECTED_RESULT="Hello from collections api"
 
 echo ""
-echo $RESPONSE
+echo "Response from ${ALPHA_SERVER} is \"$RESPONSE\" "
+[ "$RESPONSE" == "$EXPECTED_RESULT" ] && echo "TRUE : correct response " || echo "FALSE: wrong response"
 echo ""
 
 exit 0
