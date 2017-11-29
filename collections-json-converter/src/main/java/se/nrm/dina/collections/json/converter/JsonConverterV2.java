b95a21bafa32e58ced2b3ea90c9d44259c8a0161
idali0226 <ida.li@nrm.se>
2017-11-29 17:48:53 +0100

    Add exceptions

--- collections-json-converter/src/main/java/se/nrm/dina/collections/json/converter/JsonConverterV2.java
+++ collections-json-converter/src/main/java/se/nrm/dina/collections/json/converter/JsonConverterV2.java
@@ -6,6 +6,7 @@
 package se.nrm.dina.collections.json.converter;
 
 import java.util.List;
+import javax.json.JsonArray;
 import javax.json.JsonObject; 
 import se.nrm.dina.collections.data.model.impl.IndividualGroup; 
 import se.nrm.dina.collections.exceptions.CollectionsException;
@@ -28,6 +29,12 @@ public interface JsonConverterV2<T extends Object> {
 //    JsonObject convert(T bean);
     
     JsonObject convertSuccessMessage(String message);
+    
+    JsonObject readInJson(String json);
+    
+    JsonObject getAttributes(JsonObject json);
+    
+    JsonArray getJsonArray(JsonObject json, String key);
 
     JsonObject convertError(CollectionsException error);
     
